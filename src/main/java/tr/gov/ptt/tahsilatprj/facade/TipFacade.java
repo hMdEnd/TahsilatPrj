4e81094e9e5683b6f6b835b74b82aa01d4cc7e97
Administrator <Administrator@10.0.0.74>
2015-09-01 11:23:20 +0300

    sessionbeanler oluşturuldu

---
+++ src/main/java/tr/gov/ptt/tahsilatprj/facade/TipFacade.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tr.gov.ptt.tahsilatprj.facade;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import tr.gov.ptt.tahsilatprj.entity.Tip;
+
+/**
+ *
+ * @author Administrator
+ */
+@Stateless
+public class TipFacade extends AbstractFacade<Tip> {
+    @PersistenceContext(unitName = "tr.gov.ptt_TahsilatPrj_war_1.0-SNAPSHOTPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public TipFacade() {
+        super(Tip.class);
+    }
+    
+}
